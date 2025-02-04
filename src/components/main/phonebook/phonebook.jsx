135858aa0efdf0e0b1c9aa59bf367cf24d008478
ivnlsv <ivan.losev@denuo.legal>
2025-02-04 11:16:21 +0300

    Update

--- src/components/main/phonebook/phonebook.jsx
+++ src/components/main/phonebook/phonebook.jsx
@@ -1,4 +1,4 @@
-import css from './phonebook.module.scss'
+import css from './phonebook.module.scss';
 import { useState } from 'react';
 import { userData } from '../../../data/userData';
 
@@ -9,7 +9,13 @@ export const Phonebook = () => {
 	const handleSearchChange = (e) => {
 		setSearchTerm(e.target.value);
 	};
-  const columns = ['Name', 'Job position', 'Extension', 'Mobile phone', 'Office'] 
+	const columns = [
+		'Name',
+		'Job position',
+		'Extension',
+		'Mobile phone',
+		'Office',
+	];
 	const filteredContacts = contacts.filter(
 		(contact) =>
 			(contact.name &&
@@ -21,62 +27,45 @@ export const Phonebook = () => {
 				contact.location.toLowerCase().includes(searchTerm.toLowerCase()))
 	);
 
-  return (
-    <main>
-      <div className={css.layout}>
-        <div className={css.searchBlock}>
-        <div className={css.searchbox}>
-        <input
-            type='text'
-            placeholder='Search...'
-            value={searchTerm}
-            onChange={handleSearchChange}
-            className={css.input}
-				/>
-        </div>
-        </div>
-              <div className={css.dataBlock}>
-          <div className={css.dataColumns}>
-            {columns.map((column, index) => (
-              <div key={index}>{column}</div>
-            ))}
-          </div>
-          <div></div>
-          {filteredContacts.length > 0 ? (
-            filteredContacts.map((contact) => (
-              <div key={contact.id} className={css.columnItem}>
-								<div>{contact.name}</div>
-								<div>{contact.position}</div>
-                <div>{contact.phone}</div>
-                <div>{contact.mobile}</div>
-								<div>{contact.location}</div>
-							</div>
-						))
-					) : (
-						<div className='text-center'>Нет совпадений</div>
-					)}
+	return (
+		<main>
+			<div className={css.layout}>
+				<div className={css.searchBlock}>
+					<div className={css.searchbox}>
+						<input
+							type='text'
+							placeholder='Search...'
+							value={searchTerm}
+							onChange={handleSearchChange}
+							className={css.input}
+						/>
+					</div>
 				</div>
-      </div>
-      <div className={css.column}>
-        {columns.map((column, index) => (
-          <div className={css.header} key={index}>{column}     
-          </div>
-      ))}
-         {filteredContacts.length > 0 ? (
-            filteredContacts.map((contact) => (
-              <div key={contact.id} className={css.columnItem}>
-								<div>{contact.name}</div>
-								<div>{contact.position}</div>
-                <div>{contact.phone}</div>
-                <div>{contact.mobile}</div>
-								<div>{contact.location}</div>
-							</div>
-						))
-					) : (
-						<div className='text-center'>Нет совпадений</div>
-					)}     
-        
-      </div>
+				
+			</div>
+			<div className={css.column}>
+				<div className={css.columnHeader}>
+					{columns.map((column, index) => (
+						<div className={css.header} key={index}>
+							{column}
+						</div>
+					))}
+				</div>
+
+				{filteredContacts.length > 0 ? (
+					filteredContacts.map((contact) => (
+						<div key={contact.id} className={css.columnElem}>
+							<div className={css.columnCell}>{contact.name}</div>
+							<div className={css.columnCell}>{contact.position}</div>
+							<div className={css.columnCell}>{contact.phone}</div>
+							<div className={css.columnCell}>{contact.mobile}</div>
+							<div className={css.columnCell}>{contact.location}</div>
+						</div>
+					))
+				) : (
+					<div className='text-center'>Нет совпадений</div>
+				)}
+			</div>
 		</main>
 	);
 };
